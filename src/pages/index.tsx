0cc353f3b0c5e818fcf9cad7eb0b64c89761e419
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-10-18 19:22:36 +0900

    set script and css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,52 @@
+import { useEffect } from "react"
 import Head from "next/head"
-import { Button } from "@mantine/core"
+import { Box } from "@mantine/core"
 
 import type { NextPage } from "next"
 
+const loadScript = (url: string, onload: () => void) => {
+  const head = document.getElementsByTagName("head")[0] as HTMLElement
+  const script = document.createElement("script")
+  script.type = "text/javascript"
+  script.src = url
+  script.addEventListener("load", onload)
+
+  head.appendChild(script)
+}
+
+const loadCss = (url: string) => {
+  const head = document.getElementsByTagName("head")[0] as HTMLElement
+  const link = document.createElement("link")
+  link.rel = "stylesheet"
+  link.href = url
+
+  head.appendChild(link)
+}
+
 const Index: NextPage = () => {
+  useEffect(() => {
+    let mapquestSrc = "https://api.mapbox.com/mapbox-gl-js/v1.13.2/mapbox-gl.js"
+    let mapboxSrc = "https://prodmqpstorage.z11.web.core.windows.net/mqplatform.js"
+    let mapboxCssHref = "https://api.mapbox.com/mapbox-gl-js/v1.13.2/mapbox-gl.css"
+
+    loadScript(mapquestSrc, () => {
+      loadScript(mapboxSrc, () => {
+        // @ts-ignore
+        const transformRequest = mqplatformTransformRequest("63da4d3c414e4ae59b7af3d654fefaff")
+        // @ts-ignore
+        var map = new mapboxgl.Map({
+          container: "map",
+          style: "mqplatform://maps-api/styles/v1/18",
+          transformRequest: transformRequest,
+        })
+        // @ts-ignore
+        map.addControl(new mapboxgl.NavigationControl())
+      })
+    })
+
+    loadCss(mapboxCssHref)
+  }, [])
+
   return (
     <>
       <Head>
