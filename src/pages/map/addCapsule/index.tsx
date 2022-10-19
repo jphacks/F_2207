f57ae1e1ba1f14ff4cc6c24e293374889d98a419
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-10-19 23:58:45 +0900

    show gps selector

--- src/pages/map/addCapsule/index.tsx
+++ src/pages/map/addCapsule/index.tsx
@@ -1,14 +1,35 @@
-import { Box } from "@mantine/core"
+import { Box, Text, useMantineTheme } from "@mantine/core"
 
-import CapsuleColorSelector from "@/view/ CapsuleColorSelector"
+import ColorSelector from "@/view/ CapsuleColorSelector"
 
 import type { NextPage } from "next"
 
 const CapsuleAdd: NextPage = () => {
+  const theme = useMantineTheme()
+  const capsuleColors = [
+    theme.colors["brand"][3],
+    "#9581F2",
+    "#F1ABDD",
+    "#EB5040",
+    "#DE6437",
+    "#F8D551",
+    "#6BE58B",
+    "#73E4E3",
+    "#2351D5",
+    "#000000",
+    "#D3DAE1",
+    "#FFFFFF",
+  ]
+
+  const gpsColors = ["#000000", "#FFFFFF"]
+
   return (
     <Box>
-      カプセル追加画面
-      <CapsuleColorSelector />
+      <Text>カプセルを作ろう</Text>
+      <Text>カプセルの色</Text>
+      <ColorSelector colors={capsuleColors} onSelect={() => {}} />
+      <Text>GPSの色</Text>
+      <ColorSelector colors={gpsColors} onSelect={() => {}} />
     </Box>
   )
 }
--- src/view/ CapsuleColorSelector.tsx
+++ src/view/ CapsuleColorSelector.tsx
@@ -1,34 +1,28 @@
 import { CheckIcon, ColorSwatch, SimpleGrid, useMantineTheme } from "@mantine/core"
 import { useState } from "react"
 
-const CapsuleColorSelector = () => {
+type ColorSelectorProps = {
+  colors: string[]
+  onSelect: (color: string) => void
+}
+
+const ColorSelector: React.FC<ColorSelectorProps> = ({ colors, onSelect }) => {
   const theme = useMantineTheme()
-  const [checked, setChecked] = useState(theme.colors["brand"][3])
-  const targetColors = [
-    theme.colors["brand"][3],
-    "#9581F2",
-    "#F1ABDD",
-    "#EB5040",
-    "#DE6437",
-    "#F8D551",
-    "#6BE58B",
-    "#73E4E3",
-    "#2351D5",
-    "#000000",
-    "#D3DAE1",
-    "#FFFFFF",
-  ]
+  const [checked, setChecked] = useState(colors[0])
 
   const isDarkColor = (color: string) => {
     return color == "#2351D5" || color == "#000000"
   }
 
-  const swatches = targetColors.map((color) => (
+  const swatches = colors.map((color) => (
     <ColorSwatch
       component="button"
       key={color}
       color={color}
-      onClick={() => setChecked(color)}
+      onClick={() => {
+        setChecked(color)
+        onSelect(color)
+      }}
       sx={{ color: isDarkColor(color) ? "#fff" : "#000000", cursor: "pointer" }}
       size={40}
     >
@@ -43,4 +37,4 @@ const CapsuleColorSelector = () => {
   )
 }
 
-export default CapsuleColorSelector
+export default ColorSelector
