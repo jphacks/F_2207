d4a96ba7d31f93602d3ac272334e3b2838d7f5ba
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-10-20 00:15:51 +0900

    save color

--- src/pages/map/addCapsule/index.tsx
+++ src/pages/map/addCapsule/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Text, useMantineTheme } from "@mantine/core"
+import { useState } from "react"
 
 import ColorSelector from "@/view/ CapsuleColorSelector"
 
@@ -6,6 +7,9 @@ import type { NextPage } from "next"
 
 const CapsuleAdd: NextPage = () => {
   const theme = useMantineTheme()
+  const [capsuleColor, setCapsuleColor] = useState(theme.colors["brand"][3])
+  const [gpsColor, setGpsColor] = useState("#000000")
+
   const capsuleColors = [
     theme.colors["brand"][3],
     "#9581F2",
@@ -28,7 +32,7 @@ const CapsuleAdd: NextPage = () => {
       <Text color={"white"}>カプセルを作ろう</Text>
       <Box className="p-4">
         <Text color={"white"}>カプセルの色</Text>
-        <ColorSelector colors={capsuleColors} onSelect={() => {}} />
+        <ColorSelector colors={capsuleColors} onSelect={setCapsuleColor} />
       </Box>
       <Box className="p-4">
         <Text color={"white"}>絵文字</Text>
@@ -38,7 +42,7 @@ const CapsuleAdd: NextPage = () => {
       </Box>
       <Box className="p-4">
         <Text color={"white"}>GPSの色</Text>
-        <ColorSelector colors={gpsColors} onSelect={() => {}} />
+        <ColorSelector colors={gpsColors} onSelect={setGpsColor} />
       </Box>
     </Box>
   )
