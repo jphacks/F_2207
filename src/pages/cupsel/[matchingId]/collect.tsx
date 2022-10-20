9f11c80f4ac4434d0a5991942d55c0330516b327
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-10-20 20:45:11 +0900

    set design

--- src/pages/cupsel/[matchingId]/collect.tsx
+++ src/pages/cupsel/[matchingId]/collect.tsx
@@ -1,22 +1,33 @@
-import { Box, SimpleGrid, Text } from "@mantine/core"
+import { Box, Center, SimpleGrid, Text } from "@mantine/core"
 import { NextPage } from "next"
 import React from "react"
 
+import WalkthroughLayout from "@/view/layout/walkthrough"
+
 const Collect: NextPage = () => {
   return (
-    <div>
-      <Text>写真や動画を追加しよう</Text>
-      <Text>あなたの写真や動画</Text>
-      <SimpleGrid cols={3} spacing={3} verticalSpacing={3} sx={{ width: "75vw" }}>
-        <Box component="button" sx={{ height: "25vw", width: "25vw" }} />
-        <Box sx={{ height: "25vw", width: "25vw", background: "#ffffff" }} />
-        <Box sx={{ height: "25vw", width: "25vw", background: "#ffffff" }} />
-        <Box sx={{ height: "25vw", width: "25vw", background: "#ffffff" }} />
-        <Box sx={{ height: "25vw", width: "25vw", background: "#ffffff" }} />
-        <Box sx={{ height: "25vw", width: "25vw", background: "#ffffff" }} />
-        <Box sx={{ height: "25vw", width: "25vw", background: "#ffffff" }} />
-      </SimpleGrid>
-    </div>
+    <WalkthroughLayout
+      title="写真や動画を追加しよう"
+      totalStep={4}
+      currentStep={2}
+      onClickNext={() => {}}
+      onClickPrevOrClose={() => {}}
+    >
+      <Text color="white" weight="bold" size="sm">
+        あなたの写真や動画
+      </Text>
+      <Center>
+        <SimpleGrid className="pt-4" cols={3} spacing={3} verticalSpacing={3}>
+          <Box component="button" sx={{ height: "30vw", width: "30vw" }} />
+          <Box sx={{ height: "30vw", width: "30vw", background: "#ffffff" }} />
+          <Box sx={{ height: "30vw", width: "30vw", background: "#ffffff" }} />
+          <Box sx={{ height: "30vw", width: "30vw", background: "#ffffff" }} />
+          <Box sx={{ height: "30vw", width: "30vw", background: "#ffffff" }} />
+          <Box sx={{ height: "30vw", width: "30vw", background: "#ffffff" }} />
+          <Box sx={{ height: "30vw", width: "30vw", background: "#ffffff" }} />
+        </SimpleGrid>
+      </Center>
+    </WalkthroughLayout>
   )
 }
 
