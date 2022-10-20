3a4ba4552e4a0f4e673973ef56dcdd1426cc64fd
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-10-20 20:35:23 +0900

    add grid base

--- src/pages/cupsel/[matchingId]/collect.tsx
+++ src/pages/cupsel/[matchingId]/collect.tsx
@@ -1,8 +1,23 @@
+import { Box, SimpleGrid, Text } from "@mantine/core"
 import { NextPage } from "next"
 import React from "react"
 
 const Collect: NextPage = () => {
-  return <div>写真や動画の投稿</div>
+  return (
+    <div>
+      <Text>写真や動画を追加しよう</Text>
+      <Text>あなたの写真や動画</Text>
+      <SimpleGrid cols={3} spacing={3} verticalSpacing={3} sx={{ width: "75vw" }}>
+        <Box component="button" sx={{ height: "25vw", width: "25vw" }} />
+        <Box sx={{ height: "25vw", width: "25vw", background: "#ffffff" }} />
+        <Box sx={{ height: "25vw", width: "25vw", background: "#ffffff" }} />
+        <Box sx={{ height: "25vw", width: "25vw", background: "#ffffff" }} />
+        <Box sx={{ height: "25vw", width: "25vw", background: "#ffffff" }} />
+        <Box sx={{ height: "25vw", width: "25vw", background: "#ffffff" }} />
+        <Box sx={{ height: "25vw", width: "25vw", background: "#ffffff" }} />
+      </SimpleGrid>
+    </div>
+  )
 }
 
 export default Collect
