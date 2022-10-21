00e4861c7b7d06bb308df9a024559e131c998dce
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-10-21 22:44:47 +0900

    set style

--- src/pages/cupsel/[matchingId]/register.tsx
+++ src/pages/cupsel/[matchingId]/register.tsx
@@ -1,4 +1,4 @@
-import { Text, Textarea, TextInput } from "@mantine/core"
+import { Text, Textarea, TextInput, useMantineTheme } from "@mantine/core"
 import { DatePicker } from "@mantine/dates"
 import { AiOutlineEdit, AiOutlineLock, AiOutlineMessage } from "react-icons/ai"
 import { NextPage } from "next"
@@ -8,6 +8,7 @@ import WalkthroughLayout from "@/view/layout/walkthrough"
 import CapsulePreview from "@/view/CapsulePreview"
 
 const Register: NextPage = () => {
+  const theme = useMantineTheme()
   return (
     <WalkthroughLayout
       title="カプセルを作ろう"
@@ -17,12 +18,36 @@ const Register: NextPage = () => {
       onClickPrevOrClose={null}
     >
       <CapsulePreview capsuleColor={"#ffffff"} gpsColor={"#000000"} emoji={"😄"} lng={0} lat={0} />
-      <Text color="white" weight="bold" size="sm">
+      <Text className="pb-4" color="white" weight="bold" size="sm">
         カプセルの情報
       </Text>
-      <TextInput placeholder="タイトルを書く（最大18字）" icon={<AiOutlineEdit />} />
-      <DatePicker placeholder="開封できる日を指定する" icon={<AiOutlineLock />} />
-      <Textarea placeholder="メモを残す" icon={<AiOutlineMessage />} />
+      <TextInput
+        className="pb-3"
+        placeholder="タイトルを書く（最大18字）"
+        icon={<AiOutlineEdit size={24} color={theme.colors.gray[0]} />}
+        variant="filled"
+        size="lg"
+        iconWidth={48}
+      />
+      <DatePicker
+        className="pb-3"
+        placeholder="開封できる日を指定する"
+        icon={<AiOutlineLock size={24} color={theme.colors.gray[0]} />}
+        dropdownType="modal"
+        variant="filled"
+        size="lg"
+        iconWidth={48}
+        styles={(theme) => ({ input: { "&::placeholder": { color: theme.colors.gray[0] } } })}
+      />
+      <Textarea
+        placeholder="メモを残す"
+        icon={<AiOutlineMessage size={24} color={theme.colors.gray[0]} />}
+        minRows={9}
+        variant="filled"
+        size="lg"
+        iconWidth={48}
+        styles={() => ({ icon: { top: 12, bottom: "initial" } })}
+      />
     </WalkthroughLayout>
   )
 }
