2a675515d6aff85ed4ce983760bc459a17b6f2e8
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2022-10-21 21:58:30 +0900

    add input area

--- src/pages/cupsel/[matchingId]/register.tsx
+++ src/pages/cupsel/[matchingId]/register.tsx
@@ -1,8 +1,29 @@
+import { Text, Textarea, TextInput } from "@mantine/core"
+import { DatePicker } from "@mantine/dates"
 import { NextPage } from "next"
 import React from "react"
 
+import WalkthroughLayout from "@/view/layout/walkthrough"
+import CapsulePreview from "@/view/CapsulePreview"
+
 const Register: NextPage = () => {
-  return <div>カプセル情報登録</div>
+  return (
+    <WalkthroughLayout
+      title="カプセルを作ろう"
+      totalStep={4}
+      currentStep={3}
+      onClickNext={null}
+      onClickPrevOrClose={null}
+    >
+      <CapsulePreview capsuleColor={"#ffffff"} gpsColor={"#000000"} emoji={"😄"} lng={0} lat={0} />
+      <Text color="white" weight="bold" size="sm">
+        カプセルの情報
+      </Text>
+      <TextInput placeholder="タイトルを書く（最大18字）" />
+      <DatePicker placeholder="開封できる日を指定する" />
+      <Textarea placeholder="メモを残す" />
+    </WalkthroughLayout>
+  )
 }
 
 export default Register
