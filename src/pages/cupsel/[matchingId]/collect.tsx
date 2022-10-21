a6e134a748f75e2f63fd9b5432eb5c26d5c7e096
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-10-21 23:06:28 +0900

    feat: collect page firestore integration

--- src/pages/cupsel/[matchingId]/collect.tsx
+++ src/pages/cupsel/[matchingId]/collect.tsx
@@ -9,41 +9,73 @@ import {
   useMantineTheme,
 } from "@mantine/core"
 import { NextPage } from "next"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { FiPlusCircle } from "react-icons/fi"
 import { useRouter } from "next/router"
 
 import WalkthroughLayout from "@/view/layout/walkthrough"
 import UserAvater from "@/view/UserAvater"
+import { useMatchingUsers, useMatchingWithRedirect } from "@/hooks/useMatching"
+import { listenItemCount, postItem } from "@/repository/items"
+import { useUser } from "@/auth/useAuth"
+
+const useItemCount = (matchingId: string) => {
+  const [postedItemCount, setPostedItemCount] = useState<Record<string, number>>({})
+
+  useEffect(() => {
+    const unsubscribe = listenItemCount(matchingId, (postedItem) => {
+      setPostedItemCount((prev) => {
+        const newItemCount = { ...prev }
+        for (const userId of Object.keys(postedItem)) {
+          newItemCount[userId] = (newItemCount[userId] ?? 0) + postedItem[userId]
+        }
+        return newItemCount
+      })
+    })
+    return unsubscribe
+  }, [matchingId])
+
+  return postedItemCount
+}
 
 const Collect: NextPage = () => {
   const router = useRouter()
-  const matchingId = router.query.matchingId as string
   const theme = useMantineTheme()
+  const user = useUser()
+
+  const matchingId = router.query.matchingId as string
+  const matching = useMatchingWithRedirect(matchingId)
+  const matchingUsers = useMatchingUsers(matchingId)
+  const postedItemCount = useItemCount(matchingId)
+
   const [files, setFiles] = useState<File[]>([])
+  const [previewImages, setPreviewImages] = useState<string[]>([])
 
-  // TODO:本当はいい感じにとってくる
-  const users = [
-    {
-      id: "test",
-      name: "user name",
-      iconUrl:
-        "https://lh3.googleusercontent.com/a/ALm5wu3m4M-U62i3Z8U7BlyEJX4h4bzX4rIrhz3aI5m3=s96-c",
-    },
-  ]
+  const handleAddFiles = async (addedFiles: File[]) => {
+    if (user == null) {
+      window.alert("ログインしてください")
+      return
+    }
 
-  const addFiles = (addedFiles: File[]) => {
     setFiles((prev) => [...prev, ...addedFiles])
+
+    const previewUrls = addedFiles.map((file) => URL.createObjectURL(file))
+    setPreviewImages((prev) => [...prev, ...previewUrls])
+
+    try {
+      await postItem({ user, matchingId }, addedFiles)
+    } catch (e) {
+      window.alert("エラーが発生しました")
+    }
   }
 
-  const previews = files.map((file, index) => {
-    const imageUrl = URL.createObjectURL(file)
+  const previews = previewImages.map((url, index) => {
     return (
       <Image
-        key={index}
-        alt={file.name}
-        src={imageUrl}
-        imageProps={{ onLoad: () => URL.revokeObjectURL(imageUrl) }}
+        key={url}
+        alt=""
+        src={url}
+        imageProps={{ onLoad: () => URL.revokeObjectURL(url) }}
         styles={{
           figure: {
             width: "100%",
@@ -75,8 +107,12 @@ const Collect: NextPage = () => {
     >
       <Box className="pt-10 pb-16">
         <Group spacing={10}>
-          {users.map((user) => (
-            <UserAvater key={user.id} user={user} label={12} />
+          {matchingUsers.map((matchingUser) => (
+            <UserAvater
+              key={matchingUser.id}
+              user={matchingUser}
+              label={postedItemCount[matchingUser.id] ?? 0}
+            />
           ))}
         </Group>
       </Box>
@@ -85,7 +121,12 @@ const Collect: NextPage = () => {
       </Text>
       <Center>
         <SimpleGrid className="w-full pt-4" cols={3} spacing={3} verticalSpacing={3}>
-          <FileButton key="addButton" onChange={addFiles} accept="image/png,image/jpeg" multiple>
+          <FileButton
+            key="addButton"
+            onChange={handleAddFiles}
+            accept="image/png,image/jpeg"
+            multiple
+          >
             {(props) => (
               <Box
                 component="button"
@@ -96,6 +137,7 @@ const Collect: NextPage = () => {
                   backgroundColor: theme.colors.gray[8],
                   border: "none",
                   aspectRatio: "1 / 1",
+                  transition: "backgroundColor 1s",
                   "&:hover": {
                     backgroundColor: theme.colors.gray[6],
                     cursor: "pointer",
