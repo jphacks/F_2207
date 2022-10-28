054fc8374584726e5ec47efcf7594741637c1e8c
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-10-28 21:25:17 +0900

    feat(marketplace): 3d object map

--- src/view/map/Map.tsx
+++ src/view/map/Map.tsx
@@ -4,8 +4,17 @@ import axios from "axios"
 import { useRouter } from "next/router"
 import Head from "next/head"
 import { createRoot } from "react-dom/client"
-import { Map, NavigationControl, GeolocateControl, Marker, Popup, LngLatLike } from "mapbox-gl"
-import "mapbox-gl/dist/mapbox-gl.css"
+import {
+  Map,
+  NavigationControl,
+  GeolocateControl,
+  Marker,
+  Popup,
+  MercatorCoordinate,
+  LngLatLike,
+} from "mapbox-gl"
+import THREE, { Camera, Scene, DirectionalLight, WebGLRenderer, Matrix4, Vector3 } from "three"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 import { MapBoxClick } from "@/types/mapBoxClick"
 import { Feature } from "@/types/feature"
@@ -21,6 +30,8 @@ import LockedCapsule from "./LockedCapsule"
 export type MapPageProps = {
   selectedCapsuleCenter: LngLatLike | null
 }
+
+import "mapbox-gl/dist/mapbox-gl.css"
 
 const MapPage: React.FC<MapPageProps> = ({ selectedCapsuleCenter }) => {
   const user = useUser()
@@ -71,7 +82,9 @@ const MapPage: React.FC<MapPageProps> = ({ selectedCapsuleCenter }) => {
         lat: currentPosition.latitude,
         lng: currentPosition.longitude,
       },
-      zoom: 15,
+      zoom: 17,
+      bearing: -12,
+      pitch: 60,
     })
     mapRef.current = map
 
@@ -92,8 +105,174 @@ const MapPage: React.FC<MapPageProps> = ({ selectedCapsuleCenter }) => {
       setCenterToCurrentPlace(map)
     })
 
-    map.on("load", () => {
+    // parameters to ensure the model is georeferenced correctly on the map
+    const modelOrigin = [135.6042317745411, 34.86210390210311] as const
+    const modelAltitude = 0
+    const modelRotate = [Math.PI / 2, 0, 0]
+
+    const modelAsMercatorCoordinate = MercatorCoordinate.fromLngLat(modelOrigin, modelAltitude)
+
+    // transformation parameters to position, rotate and scale the 3D model onto the map
+    const modelTransform = {
+      translateX: modelAsMercatorCoordinate.x,
+      translateY: modelAsMercatorCoordinate.y,
+      translateZ: modelAsMercatorCoordinate.z,
+      rotateX: modelRotate[0],
+      rotateY: modelRotate[1],
+      rotateZ: modelRotate[2],
+      /* Since the 3D model is in real world meters, a scale transform needs to be
+       * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+       */
+      scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits(),
+    }
+
+    const camera = new Camera()
+    const scene = new Scene()
+    let renderer: THREE.WebGLRenderer | null = null
+
+    // configuration of the custom layer for a 3D model per the CustomLayerInterface
+    const customLayer: mapboxgl.AnyLayer = {
+      id: "3d-model",
+      type: "custom",
+      renderingMode: "3d",
+      onAdd: function (map, gl) {
+        // create two three.js lights to illuminate the model
+        const directionalLight = new DirectionalLight(0xffffff)
+        directionalLight.position.set(0, -70, 100).normalize()
+        scene.add(directionalLight)
+
+        const directionalLight2 = new DirectionalLight(0xffffff)
+        directionalLight2.position.set(0, 70, 100).normalize()
+        scene.add(directionalLight2)
+
+        // use the three.js GLTF loader to add the 3D model to the three.js scene
+        const loader = new GLTFLoader()
+        loader.load("/models/capsule3.glb", (gltf) => {
+          scene.add(gltf.scene)
+        })
+
+        // use the Mapbox GL JS map canvas for three.js
+        renderer = new WebGLRenderer({
+          canvas: map.getCanvas(),
+          context: gl,
+          antialias: true,
+        })
+
+        renderer!.autoClear = false
+      },
+      render: function (gl, matrix) {
+        const rotationX = new Matrix4().makeRotationAxis(
+          new Vector3(1, 0, 0),
+          modelTransform.rotateX,
+        )
+        const rotationY = new Matrix4().makeRotationAxis(
+          new Vector3(0, 1, 0),
+          modelTransform.rotateY,
+        )
+        const rotationZ = new Matrix4().makeRotationAxis(
+          new Vector3(0, 0, 1),
+          modelTransform.rotateZ,
+        )
+
+        const m = new Matrix4().fromArray(matrix)
+        const l = new Matrix4()
+          .makeTranslation(
+            modelTransform.translateX,
+            modelTransform.translateY,
+            modelTransform.translateZ as number,
+          )
+          .scale(
+            new Vector3(
+              modelTransform.scale * 0.5,
+              -modelTransform.scale * 0.5,
+              modelTransform.scale * 0.5,
+            ),
+          )
+          .multiply(rotationX)
+          .multiply(rotationY)
+          .multiply(rotationZ)
+
+        camera.projectionMatrix = m.multiply(l)
+        renderer?.resetState()
+        renderer?.render(scene, camera)
+        map.triggerRepaint()
+      },
+    }
+
+    map.on("style.load", () => {
+      map.addLayer(customLayer, "EP9PAUebGrNL2Wb3idfvISMwyVt1")
+    })
+
+    map.on("load", (ev) => {
       setMarker(map, userID)
+      console.log(map)
+
+      // map.addSource("mapbox-dem", {
+      //   type: "raster-dem",
+      //   url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+      //   tileSize: 512,
+      //   maxzoom: 14,
+      // })
+      // add the DEM source as a terrain layer with exaggerated height
+      // map.setTerrain({ source: "structurel-dem", exaggeration: 1.5 })
+
+      // // add sky styling with `setFog` that will show when the map is highly pitched
+      // map.setFog({
+      //   "horizon-blend": 0.3,
+      //   color: "#f8f0e3",
+      //   "high-color": "#add8e6",
+      //   "space-color": "#d8f2ff",
+      //   "star-intensity": 0.0,
+      // })
+      const layers = map.getStyle().layers
+      const labelLayerId = layers?.find(
+        (layer) => layer.type === "symbol" && layer.layout?.["text-field"],
+      )?.id
+
+      if (labelLayerId == null) {
+        return
+      }
+
+      // The 'building' layer in the Mapbox Streets
+      // vector tileset contains building height data
+      // from OpenStreetMap.
+      // map.addLayer(
+      //   {
+      //     id: "add-3d-buildings",
+      //     source: "composite",
+      //     "source-layer": "building",
+      //     filter: ["==", "extrude", "true"],
+      //     type: "fill-extrusion",
+      //     minzoom: 15,
+      //     paint: {
+      //       "fill-extrusion-color": "#aaa",
+
+      //       // Use an 'interpolate' expression to
+      //       // add a smooth transition effect to
+      //       // the buildings as the user zooms in.
+      //       "fill-extrusion-height": [
+      //         "interpolate",
+      //         ["linear"],
+      //         ["zoom"],
+      //         15,
+      //         0,
+      //         15.05,
+      //         ["get", "height"],
+      //       ],
+      //       "fill-extrusion-base": [
+      //         "interpolate",
+      //         ["linear"],
+      //         ["zoom"],
+      //         15,
+      //         0,
+      //         15.05,
+      //         ["get", "min_height"],
+      //       ],
+      //       "fill-extrusion-opacity": 0.6,
+      //     },
+      //   },
+      //   labelLayerId,
+      // )
     })
 
     map.on("click", userID, (e) => {
@@ -121,6 +300,7 @@ const MapPage: React.FC<MapPageProps> = ({ selectedCapsuleCenter }) => {
         clearMarker()
         res.data.forEach((layer: any) => {
           if (layer.name == userID) {
+            console.log(layer)
             // set Image
             axios
               .get(
