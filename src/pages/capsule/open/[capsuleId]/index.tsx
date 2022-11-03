e93d0b7115abc3d21d634b0c85adc4bd03265e24
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-11-03 22:43:56 +0900

    fix: format error

--- src/pages/capsule/open/[capsuleId]/index.tsx
+++ src/pages/capsule/open/[capsuleId]/index.tsx
@@ -166,7 +166,7 @@ const ArPage: React.FC<{ capsule: Capsule }> = ({ capsule }) => {
           }}
         />
         {geolocation != null && (
-          <div className="absolute flex left-6 bottom-6">
+          <div className="absolute left-6 bottom-6 flex">
             <Text
               sx={{
                 display: "block",
