7808ca17fbed5c8cf969aef31cc65c5a9f6a4c01
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-11-10 19:02:37 +0900

    fix: add early return

--- src/view/map/Map.tsx
+++ src/view/map/Map.tsx
@@ -175,7 +175,11 @@ const MapPage: React.FC<MapPageProps> = ({ selectedCapsuleCenter }) => {
                 `https://prod-mqplatform-api.azure-api.net/maps-api/features/v1/18/${layer.id}?subscription_key=${process.env.NEXT_PUBLIC_MAP_SUBSCRIPTION_KEY}`,
               )
               .then((res) => {
-                const sortedFeatures = res.data.features.sort(featureSortFunc).slice(1, 3)
+                const sortedFeatures = res.data.features.sort(featureSortFunc)
+                if (sortedFeatures.length === 0) {
+                  return
+                }
+
                 sortedFeatures.forEach((feature: Feature) => {
                   const div = document.createElement("div")
                   const root = createRoot(div)
