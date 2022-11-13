424b2b24b60796ace0e8a8af06726eecb0b2ba32
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-11-13 10:34:24 +0900

    fix: ar model size

--- src/view/ar/CapsuleModel.tsx
+++ src/view/ar/CapsuleModel.tsx
@@ -36,7 +36,8 @@ const CapsuleModel: React.FC<CapsuleModelProps> = ({
     <>
       {renderDistance <= 100 && (
         <Suspense fallback={null}>
-          <CapsuleSphereGlb position={position} scale={[2, 2, 2]} color={color} onClick={onClick} />
+          {/* @ts-ignore */}
+          <CapsuleSphereGlb position={position} scale={[4, 4, 4]} color={color} onClick={onClick} />
         </Suspense>
       )}
       <mesh>
